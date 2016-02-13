4d42a3f42aff1737fca36abc195af6a207ec92d5
skarif2 <skarif2@gmail.com>
2016-02-13 06:28:59 +0600

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp'),
+	 jade = require('gulp-jade'),
+	 uglify = require('gulp-uglify'),
+	 gulpif = require('gulp-if'),
+	 sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+	 sourcemaps = require('gulp-sourcemaps'),
+	 browserSync = require('browser-sync');
+
+var env = process.env.NODE_ENV || 'development';
+var outputDir = 'builds/development';
+
+gulp.task('jade', function(){
+   var outputStyle;
+	if(env === 'development'){
+		outputStyle = true;
+	}
+	if(env === 'production'){
+		outputStyle = false;
+	}
+	return gulp.src('src/templates/*.jade')
+		.pipe(jade({pretty: outputStyle}))
+		.pipe(gulp.dest(outputDir));
+});
+
+gulp.task('js', function(){
+	return gulp.src([
+      './bower_components/jquery/dist/jquery.js',
+      './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+      './src/js/main.js'
+   ])		
+		.pipe(sourcemaps.init())
+      .pipe(concat('main.js'))
+		.pipe(gulpif(env === 'production', uglify()))
+		.pipe(gulpif(env === 'development', sourcemaps.write()))
+		.pipe(gulp.dest(outputDir + '/js'));
+   //return gulp.src('./bower_components/modernizr/modernizr.js')
+      //.pipe(gulp.dest(outputDir + '/js'));
+});
+
+gulp.task('sass', function(){
+	var outputStyle;
+	if(env === 'development'){
+		outputStyle = 'map';
+	}
+	if(env === 'production'){
+		outputStyle = 'compressed';
+	}
+	return gulp.src('src/sass/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({includePaths: ['./bower_components/bootstrap-sass/assets/stylesheets/']},{outputStyle : outputStyle}).on('error', sass.logError))
+      .pipe(concat('main.css'))
+      .pipe(gulpif(env === 'development', sourcemaps.write()))
+      .pipe(gulp.dest( outputDir + '/css'));
+});
+
+gulp.task('reloadBrowser',['jade','js','sass'], browserSync.reload);
+
+gulp.task('watch', function(){
+	browserSync({
+		server:{
+			baseDir: 'builds/development'
+		}
+	})
+	gulp.watch('src/templates/**/*.jade', ['reloadBrowser']);
+	gulp.watch('src/js/main.js', ['reloadBrowser']);
+	gulp.watch('src/sass/**/*.scss', ['reloadBrowser']);
+});
+
+gulp.task('default', ['jade', 'js', 'sass', 'reloadBrowser', 'watch']);
